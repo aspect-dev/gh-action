19df517cf246fffc2f490f772804b2974529d1bb
Stevan Kosijer <stkosijer@gmail.com>
2024-03-07 14:50:01 +0100

    Add liblab_token and github_token inputs

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,20 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const liblabToken: string = core.getInput('liblab_token')
+    const githubToken: string = core.getInput('github_token')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.setCommandEcho(true);
+    core.exportVariable('liblab_token', liblabToken);
+    core.exportVariable('github_token', githubToken);
 
     // Log the current timestamp, wait, then log the new timestamp
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
+    await wait(parseInt('10', 10))
     core.debug(new Date().toTimeString())
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('status', `The status is: ${liblabToken}`);
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
